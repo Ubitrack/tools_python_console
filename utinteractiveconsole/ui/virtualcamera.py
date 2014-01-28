6dc8e839da6c781522a3a86bcf76be7e6d41c4ee
Ulrich Eck <ulrich.eck@magicvisionlab.com>
2014-01-28 16:19:54 +1030

    draft of vhar virtualcam

---
+++ utinteractiveconsole/ui/virtualcamera.py
@@ -0,0 +1,357 @@
+""" Enaml widget for editing a list of string
+"""
+
+#-------------------------------------------------------------------------------
+#  Imports:
+#-------------------------------------------------------------------------------
+from atom.api import (Bool, List, observe, set_default, Unicode, Enum, Int, Atom, Member)
+
+from enaml.widgets.api import RawWidget
+from enaml.core.declarative import d_
+from enaml.qt.QtCore import *
+from OpenGL.GL import *
+from enaml.qt import QtOpenGL, QtCore
+
+from pyqtgraph.opengl.GLGraphicsItem import GLGraphicsItem
+
+from pyqtgraph import Vector
+import numpy as np
+
+from ubitrack.core import math, calibration
+from ubitrack.vision import vision
+from ubitrack.visualization import visualization
+
+import logging
+log = logging.getLogger(__name__)
+
+
+class QtVirtualCameraWidget(QtOpenGL.QGLWidget):
+    """
+    """
+
+    ShareWidget = None
+
+    def __init__(self, bgtexture=None, camera_pose=None, camera_intrinsics=None,
+                 cam_width=640, cam_height=480, cam_near=0.01, cam_far=10.0,
+                 parent=None):
+
+        if QtVirtualCameraWidget.ShareWidget is None:
+            ## create a dummy widget to allow sharing objects (textures, shaders, etc) between views
+            QtVirtualCameraWidget.ShareWidget = QtOpenGL.QGLWidget()
+
+        QtOpenGL.QGLWidget.__init__(self, parent, QtVirtualCameraWidget.ShareWidget)
+
+        self.setFocusPolicy(QtCore.Qt.ClickFocus)
+
+        self.bgtexture = bgtexture
+        self.camera_intrinsics = camera_intrinsics
+        self.camera_pose = camera_pose
+
+        self.camera_width = float(cam_width)
+        self.camera_height = float(cam_height)
+        self.camera_near = float(cam_near)
+        self.camera_far = float(cam_far)
+
+        self.screen_width = cam_width
+        self.screen_height = cam_height
+
+        self.items = []
+
+
+        self.noRepeatKeys = [QtCore.Qt.Key_Right, QtCore.Qt.Key_Left, QtCore.Qt.Key_Up, QtCore.Qt.Key_Down, QtCore.Qt.Key_PageUp, QtCore.Qt.Key_PageDown]
+        self.keysPressed = {}
+        self.keyTimer = QtCore.QTimer()
+        self.keyTimer.timeout.connect(self.evalKeyState)
+
+        self.makeCurrent()
+
+
+
+    def addItem(self, item):
+        self.items.append(item)
+        if hasattr(item, 'initializeGL'):
+            self.makeCurrent()
+            try:
+                item.initializeGL()
+            except:
+                self.checkOpenGLVersion('Error while adding item %s to GLViewWidget.' % str(item))
+
+        item._setView(self)
+        self.update()
+
+    def removeItem(self, item):
+        self.items.remove(item)
+        item._setView(None)
+        self.update()
+
+
+    def setItems(self, items):
+        for item in self.items:
+            self.removeItem(item)
+
+        for item in items:
+            self.addItem(item)
+
+
+    def setCameraParameters(self, cam_width, cam_height, cam_near, cam_far):
+        self.camera_width = float(cam_width)
+        self.camera_height = float(cam_height)
+        self.camera_near = float(cam_near)
+        self.camera_far = float(cam_far)
+
+    def updateCameraIntrinsics(self, camera_intrinsics):
+        self.camera_intrinsics = camera_intrinsics.get()
+
+    def updateCameraPose(self, camera_pose):
+        self.camera_pose = camera_pose.get()
+
+    def updateBackgroundTexture(self, image):
+        if self.bgtexture is None:
+            self.bgtexture = visualization.BackgroundImage()
+        print " XXX %s " % image.time()
+        self.bgtexture.imageIn(image)
+
+
+    def initializeGL(self):
+        glClearColor(0.0, 0.0, 0.0, 0.0)
+        self.resizeGL(self.width(), self.height())
+
+    def resizeGL(self, w, h):
+        glViewport(0, 0, w, h)
+        self.screen_width = w
+        self.screen_height = h
+        #self.update()
+
+
+    def setProjection(self):
+        ## Create the projection matrix
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        if self.camera_intrinsics is not None:
+            proj = calibration.projectionMatrix3x3ToOpenGL(0., self.camera_width, 0., self.camera_height, self.camera_near, self.camera_far, self.camera_intrinsics)
+            glMultMatrixd(proj)
+
+
+    def setModelview(self):
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+        if self.camera_pose is not None:
+            pose = self.camera_pose.invert().toMatrix()
+            glMultMatrixd(pose)
+
+
+    def paintGL(self):
+        self.setProjection()
+        self.setModelview()
+        glClear( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT )
+
+        if self.bgtexture is not None:
+            self.bgtexture.draw(self.screen_width, self.screen_height)
+
+        self.drawItemTree()
+
+
+    def drawItemTree(self, item=None):
+        if item is None:
+            items = [x for x in self.items if x.parentItem() is None]
+        else:
+            items = item.childItems()
+            items.append(item)
+        items.sort(lambda a,b: cmp(a.depthValue(), b.depthValue()))
+        for i in items:
+            if not i.visible():
+                continue
+            if i is item:
+                try:
+                    glPushAttrib(GL_ALL_ATTRIB_BITS)
+                    i.paint()
+                except:
+                    import pyqtgraph.debug
+                    pyqtgraph.debug.printExc()
+                    msg = "Error while drawing item %s." % str(item)
+                    ver = glGetString(GL_VERSION)
+                    if ver is not None:
+                        ver = ver.split()[0]
+                        if int(ver.split('.')[0]) < 2:
+                            print(msg + " The original exception is printed above; however, pyqtgraph requires OpenGL version 2.0 or greater for many of its 3D features and your OpenGL version is %s. Installing updated display drivers may resolve this issue." % ver)
+                        else:
+                            print(msg)
+
+                finally:
+                    glPopAttrib()
+            else:
+                glMatrixMode(GL_MODELVIEW)
+                glPushMatrix()
+                try:
+                    tr = i.transform()
+                    a = np.array(tr.copyDataTo()).reshape((4,4))
+                    glMultMatrixf(a.transpose())
+                    self.drawItemTree(i)
+                finally:
+                    glMatrixMode(GL_MODELVIEW)
+                    glPopMatrix()
+
+
+    def mousePressEvent(self, ev):
+        self.mousePos = ev.pos()
+
+    def mouseMoveEvent(self, ev):
+        pass
+
+    def mouseReleaseEvent(self, ev):
+        pass
+
+    def wheelEvent(self, ev):
+        pass
+
+    def keyPressEvent(self, ev):
+        if ev.key() in self.noRepeatKeys:
+            ev.accept()
+            if ev.isAutoRepeat():
+                return
+            self.keysPressed[ev.key()] = 1
+            self.evalKeyState()
+
+    def keyReleaseEvent(self, ev):
+        if ev.key() in self.noRepeatKeys:
+            ev.accept()
+            if ev.isAutoRepeat():
+                return
+            try:
+                del self.keysPressed[ev.key()]
+            except:
+                self.keysPressed = {}
+            self.evalKeyState()
+
+    def checkOpenGLVersion(self, msg):
+        ## Only to be called from within exception handler.
+        ver = glGetString(GL_VERSION).split()[0]
+        if int(ver.split('.')[0]) < 2:
+            import pyqtgraph.debug
+            pyqtgraph.debug.printExc()
+            raise Exception(msg + " The original exception is printed above; however, utInteractiveConsole requires OpenGL version 2.0 or greater for many of its 3D features and your OpenGL version is %s. Installing updated display drivers may resolve this issue." % ver)
+        else:
+            raise
+
+
+    def evalKeyState(self):
+        speed = 2.0
+        if len(self.keysPressed) > 0:
+            for key in self.keysPressed:
+                if key == QtCore.Qt.Key_Right:
+                    pass
+                elif key == QtCore.Qt.Key_Left:
+                    pass
+                elif key == QtCore.Qt.Key_Up:
+                    pass
+                elif key == QtCore.Qt.Key_Down:
+                    pass
+                elif key == QtCore.Qt.Key_PageUp:
+                    pass
+                elif key == QtCore.Qt.Key_PageDown:
+                    pass
+                self.keyTimer.start(16)
+        else:
+            self.keyTimer.stop()
+
+
+
+
+
+
+
+
+class VirtualCameraWidget(RawWidget):
+    """ A Qt4 implementation of an Enaml ProxyVirtualCameraWidget.
+
+    """
+
+    __slots__ = '__weakref__'
+
+
+    #: The list of graphics items to be displayed
+    items = d_(List())
+
+    #: The camera_pose
+    camera_pose = d_(Member())
+    
+    #: The camera_intrinsics
+    camera_intrinsics = d_(Member())
+
+    #: The background texture
+    bgtexture = d_(Member())
+
+    #: .
+    hug_width = set_default('weak')
+    
+    #--------------------------------------------------------------------------
+    # Initialization API
+    #--------------------------------------------------------------------------
+    def create_widget(self, parent):
+        """ Create the QListView widget.
+
+        """
+        # Create the list model and accompanying controls:
+        widget = QtVirtualCameraWidget(parent=parent, bgtexture=self.bgtexture, camera_intrinsics=self.camera_intrinsics,
+                                       camera_pose=self.camera_pose)
+        for item in self.items:
+            self.add_item(widget, item)
+
+        return widget
+
+    def add_item(self, widget, item):
+        widget.addItem(item)
+
+    #--------------------------------------------------------------------------
+    # Signal Handlers
+    #--------------------------------------------------------------------------
+
+    def onDataReadyEvent(self, data):
+        """ The signal handler when data arrives.
+        """
+        widget = self.get_widget()
+        if widget is None:
+            #log.warn("VirtualCameraWidget is None")
+            return
+
+        if data is not None:
+            if data.intrinsics is not None:
+                widget.updateCameraIntrinsics(data.intrinsics)
+
+            if data.pose is not None:
+                widget.updateCameraPose(data.pose)
+
+            if data.image is not None:
+                widget.updateBackgroundTexture(data.image)
+
+        widget.updateGL()
+
+    #--------------------------------------------------------------------------
+    # VirtualCamera API
+    #--------------------------------------------------------------------------
+
+    def set_items(self, items):
+        """
+        """
+        widget = self.get_widget()
+        widget.setItems(items)
+
+    #--------------------------------------------------------------------------
+    # Observers
+    #--------------------------------------------------------------------------
+    @observe('items', 'camera_pose', 'camera_intrinsics')
+    def _update_proxy(self, change):
+        """ An observer which sends state change to the proxy.
+
+        """
+        # The superclass handler implementation is sufficient.
+        name = change['name']
+        widget = self.get_widget()
+        if widget:
+            if name == 'items':
+                self.set_items(self.items)
+            elif name == "camera_pose":
+                widget.camera_pose = self.camera_pose
+            elif name == "camera_intrinsics":
+                widget.camera_intrinsics = self.camera_intrinsics
