5a55b5430161710ff4ecbbae33d153f95e8b387c
Ulrich Eck <ulrich.eck@magicvisionlab.com>
2014-07-11 16:00:33 +0930

    basic tde fixes

--- utinteractiveconsole/plugins/calibration/controllers/preview_time_delay_estimation.py
+++ utinteractiveconsole/plugins/calibration/controllers/preview_time_delay_estimation.py
@@ -1,4 +1,5 @@
 __author__ = 'MVL'
+import numpy as np
 from atom.api import Value, Typed
 import enaml
 from enaml.layout.api import InsertItem
@@ -9,17 +10,15 @@ from utinteractiveconsole.plugins.calibration.controller import PreviewControlle
 import logging
 log = logging.getLogger(__name__)
 
-
 class TimeDelayEstimationPreviewFactory(PreviewControllerFactory):
 
     def create(self, workspace, name, widget_parent):
         return TimeDelayEstimationPreview(parent=self.parent,
-                                        context=self.context,
-                                        config_ns=self.config_ns,
-                                        widget_parent=widget_parent,
-                                        widget_name=name,
-                                        workspace=workspace)
-
+                                          context=self.context,
+                                          config_ns=self.config_ns,
+                                          widget_parent=widget_parent,
+                                          widget_name=name,
+                                          workspace=workspace)
 class TimeDelayEstimationPreview(PreviewControllerBase):
 
     camera = Value()
@@ -30,7 +29,7 @@ class TimeDelayEstimationPreview(PreviewControllerBase):
     target_marker = Value()
     tooltip_marker = Value()
 
-    def setupPreview(self):
+    def initialize(self):
         log.info("Setup LivePreview")
         with enaml.imports():
             from .views.time_delay_estimation import TimeDelayEstimationPreviewContent
@@ -58,29 +57,56 @@ class TimeDelayEstimationPreview(PreviewControllerBase):
         self.target_marker = self.content.scene.find("target_marker")
         self.tooltip_marker = self.content.scene.find("tooltip_marker")
 
-        cfg = self.parent.current_state.config
-        facade = self.parent.current_state.facade
-        if 'master_dfg_basedir' in cfg and 'master_dfg_filename' in cfg:
-            facade.setupMaster(cfg['master_dfg_basedir'],
-                               cfg['master_dfg_filename'])
-            facade.master.startDataflow()
+        if self.facade is not None:
+            # XXX needs some improvement .. is_running does not directly relate to startDataflow ..
+            if self.facade.is_loaded:
+                self.connector_setup(dict(value=True))
+            else:
+                self.facade.observe("is_loaded", self.connector_setup)
+                self.facade.startDataflow()
 
-
-    def teardownPreview(self):
+    def teardown(self):
         log.info("Teardown LivePreview")
-        facade = self.parent.current_state.facade
-        if facade.master is not None:
-            facade.master.stopDataflow()
+        if self.facade is not None:
+            self.facade.stopDataflow()
+            self.facade.clearDataflow()
 
-        self.camera = None
-        self.renderer = None
-        self.bgtexture = None
-        self.origin_marker = None
-        self.target_marker = None
-        self.tooltip_marker = None
+        if self.connector is not None:
+            self.connector.unobserve(self.sync_source, self.handle_data)
+            self.connector.teardown(self.facade.instance)
 
-    def moduleSetupPreview(self, controller):
-        log.info("LivePreview: setup for module %s" % controller.module_name)
+        super(TimeDelayEstimationPreview, self).teardown()
 
-    def moduleTeardownPreview(self, controller):
-        log.info("LivePreview: setup for module %s" % controller.module_name)
+    def connector_setup(self, change):
+        # XXX Add SRG Verification to Controllers !!!
+        if change['value'] == True:
+            if self.connector is not None:
+                self.connector.setup(self.facade.instance)
+                self.connector.observe(self.sync_source, self.handle_data)
+
+        if self.facade is not None:
+            self.facade.unobserve("is_loaded", self.connector_setup)
+
+    def handle_data(self, c):
+        conn = self.connector
+
+        # set debug image texture for glview
+        self.renderer.enable_trigger(False)
+
+        # could be optimized to fetch only once ...
+        if conn.camera_resolution is not None:
+            self.camera.camera_width, self.camera.camera_height = conn.camera_resolution.get().astype(np.int)
+
+        if conn.camera_intrinsics is not None:
+            self.camera.camera_intrinsics = conn.camera_intrinsics.get()
+
+        self.renderer.enable_trigger(True)
+        self.bgtexture.image_in(c['value'])
+
+
+
+    def on_module_after_load(self, change):
+        log.info("LivePreview: setup for module %s" % change['value'])
+
+    def on_module_before_unload(self, change):
+        log.info("LivePreview: teardown for module %s" % change['value'])
